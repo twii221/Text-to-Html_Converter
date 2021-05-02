fa87aea82e2b5322e398ccf72e0de97fb778cd6a
shweta <shwetavp893@gmail.com>
2021-05-02 21:34:11 +0530

    Project Text to Html converter

---
+++ blog/views.py
@@ -0,0 +1,11 @@
+
+from django.shortcuts import render
+from .models import Post
+from blog.forms import PostForm
+
+def home(request):
+    form=PostForm()
+    return render(request,'blog/home.html',{'form':form})
+
+
+# Create your views here.
